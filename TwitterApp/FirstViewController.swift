6a1aafbef86e7aa1b74ce16c9eb77bd0ddf55c4e
Chris Gaffney <ChrisGaffney@tests-air-2.nvstu.edu>
2017-11-01 17:36:01 -0400

    Initial Commit

---
+++ TwitterApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TwitterApp
+//
+//  Created by Chris Gaffney on 11/1/17.
+//  Copyright © 2017 Michael Sullivan. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TwitterApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TwitterApp
+//
+//  Created by Chris Gaffney on 11/1/17.
+//  Copyright © 2017 Michael Sullivan. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
